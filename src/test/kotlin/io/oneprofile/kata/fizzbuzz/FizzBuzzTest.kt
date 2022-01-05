d39b1676a050eeaadfa6bbfa8c8636e03276f7d2
Kong To <newlight77@gmail.com>
2022-01-05 15:41:21 +0100

    added a first unit test for fizzbuzz containg 3 rule : failing test

--- src/test/kotlin/io/oneprofile/kata/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/io/oneprofile/kata/fizzbuzz/FizzBuzzTest.kt
@@ -1,11 +1,26 @@
 package io.oneprofile.kata.fizzbuzz
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class FizzBuzzTest {
 
     @Test
     fun `should display Fizz with input contains a 3`() {
+        // GIVEN
+        val input = "3"
 
+        // Act
+        val result = FizzBuzz().compute(input)
+
+        // ASSERT
+        Assertions.assertThat(result).isEqualTo("Fizz")
+
+    }
+
+    class FizzBuzz {
+        fun compute(input: String): String {
+            return ""
+        }
     }
 }
